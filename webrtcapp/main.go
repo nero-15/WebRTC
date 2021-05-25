8a085d555b6b6e43a2ffaee8f938aefd48340418
nero-15 <nero15.dev@gmail.com>
2021-05-25 22:33:55 +0900

    setting server

--- webrtcapp/main.go
+++ webrtcapp/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"io"
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }

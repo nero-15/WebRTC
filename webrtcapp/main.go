787aa6c70db5e815fd3404f1ec15d2148b1ce819
nero-15 <nero15.dev@gmail.com>
2021-05-25 22:47:56 +0900

    setting template

--- webrtcapp/main.go
+++ webrtcapp/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
-	"io"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+var templates = template.Must(template.ParseFiles("views/index.html"))
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
+		err := templates.ExecuteTemplate(w, "index.html", nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }

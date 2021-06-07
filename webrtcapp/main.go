9ebb6ba3e1356e6f0ddea7083ff12e63cdf8158b
nero-15 <nero15.dev@gmail.com>
2021-06-07 23:01:47 +0900

    rename file

--- webrtcapp/main.go
+++ webrtcapp/main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-var templates = template.Must(template.New("").Delims("[[", "]]").ParseFiles("views/camera.html", "views/rtc.html", "views/peerjs.html", "views/call.html"))
+var templates = template.Must(template.New("").Delims("[[", "]]").ParseFiles("views/camera.html", "views/rtcdatachannel.html", "views/peerjs.html", "views/call.html"))
 
 func main() {
 
@@ -22,7 +22,7 @@ func main() {
 	//  WebRTC API の RTCDataChannel を使ったサンプル。以下のチュートリアルを参考に実装。
 	// https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Simple_RTCDataChannel_sample
 	http.HandleFunc("/RTCDataChannel_sample", func(w http.ResponseWriter, r *http.Request) {
-		err := templates.ExecuteTemplate(w, "rtc.html", nil)
+		err := templates.ExecuteTemplate(w, "rtcdatachannel.html", nil)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
